40c317b7faa1a0a3a6bfc894ec10dd4082081f98
Kiketo <kike.fernandez@ehu.es>
2014-10-23 15:39:54 +0200

    eguneratu

--- templates/scripts/init_system.sh
+++ templates/scripts/init_system.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 /home/newsreader/update_nlp_components_boss.sh
-pdsh -w _WORKER_NAME_[0-_NUM_NODES_] /home/newsreader/update_nlp_components_worker.sh
+pdsh -w _WORKER_NAME_ /home/newsreader/update_nlp_components_worker.sh
 puppet agent --test
-pdsh -w _WORKER_NAME_[0-_NUM_NODES_] puppet agent --test
-
-
-sleep 30
-su - newsreader -c 'cd /home/newsreader/opt/topologies_cluster; /opt/storm/bin/storm jar newsreader-pipe-topology.jar newsreader.pipe.topology.Main topology.conf newsreader_pipeline'
+pdsh -w _WORKER_NAME_ puppet agent --test
+pdsh -w _WORKER_NAME_ /sbin/chkconfig puppet on

fdb0f6fa55303221ec5ddf03620849581dee375c
Kiketo <kike.fernandez@ehu.es>
2014-10-27 15:54:26 +0100

    puppet zerbitzua aktibatu boss-ean ere bai

--- templates/scripts/init_system.sh
+++ templates/scripts/init_system.sh
@@ -4,4 +4,5 @@
 pdsh -w _WORKER_NAME_ /home/newsreader/update_nlp_components_worker.sh
 puppet agent --test
 pdsh -w _WORKER_NAME_ puppet agent --test
+/sbin/chkconfig puppet on
 pdsh -w _WORKER_NAME_ /sbin/chkconfig puppet on
